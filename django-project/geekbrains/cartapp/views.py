ad6c3757d71645a0440b392c4e0944082788471a
Andrey Kiselev <ezhichek@yandex-team.ru>
2019-04-07 09:12:26 +0300

    update buttons

--- django-project/geekbrains/cartapp/views.py
+++ django-project/geekbrains/cartapp/views.py
@@ -1,21 +1,24 @@
 from django.shortcuts import render, \
     get_object_or_404
 from django.http import HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
 
 from cartapp.models import ShopCart
 from mainapp.models import Products
 
 
+@login_required(login_url='/auth/')
 def show(request):
     title = 'Все товары | Корзина'
     cart = ShopCart.objects.select_related(
-            'product'
-        ).filter(user=request.user).all()
+        'product'
+    ).filter(user=request.user).all()
     return render(request, 'cartapp/cart.html',
                   {'title': title,
                    'cart': cart, })
 
 
+@login_required(login_url='/auth/')
 def add(request, pk):
     product = get_object_or_404(Products, pk=pk)
     cart = ShopCart.objects.filter(
@@ -31,6 +34,7 @@ def add(request, pk):
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
 
+@login_required(login_url='/auth/')
 def delete(request, pk):
     product = get_object_or_404(Products, pk=pk)
     cart = ShopCart.objects.filter(
