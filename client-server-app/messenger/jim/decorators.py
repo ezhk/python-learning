fecbefdca416d2caede5209b159798cdffb9f704
Andrey Kiselev <ezhik@ezhik.info>
2019-09-22 14:11:48 +0300

    log decorator has beed added

---
+++ client-server-app/messenger/jim/decorators.py
@@ -0,0 +1,40 @@
+import __main__ as main
+
+from logging import getLogger
+from . import logger
+
+
+def log(func):
+    role = "server" if "server" in main.__file__ else "client"
+    logger = getLogger(f"messenger.{role}")
+
+    print(logger.name)
+
+    def wrapper(*args, **kwargs):
+        logger.info(
+            f"Вызвана функция {func.__name__} с аргументами {args}, "
+            f"{kwargs} в модуле {func.__module__}"
+        )
+        return func(*args, **kwargs)
+
+    return wrapper
+
+
+class Log:
+    """
+    Class works like a @log decorator,
+    only as example.
+    """
+
+    def __init__(self, function):
+        self.function = function
+
+        role = "server" if "server" in main.__file__ else "client"
+        self.logger = getLogger(f"messenger.{role}")
+
+    def __call__(self, *args, **kwargs):
+        self.logger.info(
+            f"Вызвана функция {self.function.__name__} с аргументами {args}, "
+            f"{kwargs} в модуле {self.function.__module__}"
+        )
+        return self.function(*args, **kwargs)
