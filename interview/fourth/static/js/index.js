39bc5fb016868aa7e83b0183439dece08d3efc16
Andrey Kiselev <ezhik@ezhik.info>
2020-05-11 13:48:43 +0300

    Hide form on outside click

--- interview/fourth/static/js/index.js
+++ interview/fourth/static/js/index.js
@@ -1,4 +1,14 @@
 $(document).ready(() => {
+  function showForm() {
+    document.querySelector(".product-form").removeAttribute("hidden");
+    document.querySelector(".product-view").classList.add("form-pop-filter");
+  }
+
+  function hideForm() {
+    document.querySelector(".product-form").setAttribute("hidden", "hidden");
+    document.querySelector(".product-view").classList.remove("form-pop-filter");
+  }
+
   /*
     Get form data and apply classes,
     it needs for correctly suggest work on POST wrong form.
@@ -9,11 +19,11 @@ $(document).ready(() => {
 
       success: (data) => {
         $("form").replaceWith(data);
-        document.querySelector(".product-form").removeAttribute("hidden");
-        document.querySelector(".product-view").classList.add("form-pop-filter");
+        showForm();
       },
     });
   });
+
 
   // POST request form action
   $("#save-product").on("click", (e) => {
@@ -25,13 +35,22 @@ $(document).ready(() => {
 
       success: (data) => {
         $("table").html(data);
-        document.querySelector(".product-form").setAttribute("hidden", "hidden");
-        document.querySelector(".product-view").classList.remove("form-pop-filter");
+        hideForm();
       },
 
       error: (data) => {
         $("form").html(data.responseText);
       },
     });
+  });
+
+  // Hide form outside click
+  $(document).click(function(e) {
+    if (
+      !$(e.target).closest(".product-form").length &&
+      $("form").is(":visible")
+    ) {
+      hideForm();
+    }
   });
 });
